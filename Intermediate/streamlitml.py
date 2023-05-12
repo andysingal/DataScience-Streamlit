9802e5b455fe2256afef24d9a2b29f42a362e12b
andysingal <andysingal@gmail.com>
2023-05-12 16:45:02 +0530

    2

--- Intermediate/streamlitml.py
+++ Intermediate/streamlitml.py
@@ -14,11 +14,11 @@ st.write("This app uses 6 inputs to predict the species of penguin using "
          "a model built on the Palmer's Penguin's dataset. Use the form below" 
 
          " to get started!")
-password_guess = st.text_input('What is the Password?')
+# password_guess = st.text_input('What is the Password?')
 
-if password_guess != 'streamlit_password':
-
-         st.stop()
+# if password_guess != st.secrets["password"]:
+#
+#       st.stop()
 
 penguin_file = st.file_uploader('Upload your own penguin data')
 
